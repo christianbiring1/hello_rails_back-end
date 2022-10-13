84a5cd513f3b6c13b1c421209cb64ebd618ecc3c
christian <christianbiringanine22@gmail.com>
2022-10-13 15:41:37 +0200

    Fix the error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cokkie_store, :key: "_react_on_rails_app", domain: "https://chris-react-on-rails-backend.herokuapp.com"
+Rails.application.config.session_store :cookie_store, key: "_react_on_rails_app", domain: "https://chris-react-on-rails-backend.herokuapp.com"
