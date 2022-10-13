707930786c1dbf1eadaa8f4809b6c53564ea41cf
christian <christianbiringanine22@gmail.com>
2022-10-13 15:18:39 +0200

    Allow the front-end to access the resource method on the deployed app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,4 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
+  allow do
+    origins "https://chris-react-on-rails.herokuapp.com/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
 end
